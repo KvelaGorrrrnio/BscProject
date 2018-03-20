a9d1e0ee57cafb6c4ef1ee33757ade4ef023e2f2
Lars <larsvadgaard@gmail.com>
2018-03-20 14:35:51 +0100

    Added Show to ProgError.

--- src/RL/Error.hs
+++ src/RL/Error.hs
@@ -12,5 +12,14 @@ data ProgError
   | WrongType Type
   | DivByZero
   | DivHasRest
-  deriving Show
+
+instance Show ProgError where
+  show (IndexOnNonList n)   = "Tried to index on variable '" ++ n ++ "' which is not a list."
+  show IndexNotInteger      = "Index must be an integer."
+  show TestNotBoolean       = "Test must be boolean."
+  show AssertionNotBoolan   = "Assertion must be boolean."
+  show AssignedVarIsOperand = "The assigned variable occurs on the right-hand side of the binding."
+  show (WrongType t)        = "Expected type " ++ t ++ "."
+  show DivByZero            = "Tried to divide by zero."
+  show DivHasRest           = "Division must not have a rest."
 
