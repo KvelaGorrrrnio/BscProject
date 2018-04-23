c02af968282c16ef060fd2d3b8a8d0362e981cfa
Lars <larsvadgaard@gmail.com>
2018-04-23 21:33:20 +0200

    Ffs.

--- src/RL/HandleArgs.hs
+++ src/RL/HandleArgs.hs
@@ -16,31 +16,31 @@ data RevL
               , quiet :: Bool }
   deriving (Data,Typeable,Show,Eq)
 
-helpOutput = help "Write the output to a file."
+helpOutput = help "Write the output to the specified file"
 
 typeof = Typeof
   { file   = def &= args &= typFile
   , out    = def &= typFile &= helpOutput
-  ,json    = def &= help "Format output as JSON."
-  } &= help "Print the inferred types of the program."
+  ,json    = def &= help "Format output as JSON"
+  } &= help "Print the inferred types of the program"
 
 translate = Translate
   { file   = def &= args &= typFile
   , out    = def &= typFile &= helpOutput
-  ,json    = def &= help "Format output as JSON."
-  } &= help "Translate a RevL program to its SRevL counterpart."
+  ,json    = def &= help "Format output as JSON"
+  } &= help "Translate a RevL program to its SRevL counterpart"
 
 invert_ = Invert
   { file   = def &= args &= typFile
   , out    = def &= typFile &= helpOutput
-  ,json    = def &= help "Format output as JSON."
+  ,json    = def &= help "Format output as JSON"
   } &= help "Invert a RevL program"
 
 interpret = Run
-  {log     = def &= help "Output log instead of final state."
-  ,json    = def &= help "Format output as JSON."
+  {log     = def &= help "Output log instead of final state"
+  ,json    = def &= help "Format output as JSON"
   ,out     = def &= typFile &= helpOutput
-  ,quiet   = def &= help "Hide the result of the program."
+  ,quiet   = def &= help "Hide the result of the program"
   ,file    = def &= args &= typFile
   } &= help "Interpret a RevL program" &= auto
 
