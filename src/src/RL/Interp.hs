22f83253e4ddd9f9ee6c8f0464f12272594d270b
Lars <larsvadgaard@gmail.com>
2018-04-25 12:18:13 +0200

    Checking froms.

--- src/src/RL/Interp.hs
+++ src/src/RL/Interp.hs
@@ -7,6 +7,14 @@ import Common.Interp
 import Common.Log
 
 import Control.Monad.Reader
+import Control.Monad.Except
+
+
+
+import Debug.Trace
+
+
+
 
 -- The program state
 type ProgState = ReaderT AST VarState
@@ -18,18 +26,31 @@ type ProgState = ReaderT AST VarState
 runProgramWith :: AST -> VarTab -> (Either Error VarTab, Log)
 runProgramWith ast vtab = do
   let entry = getEntry ast
-  execVarState vtab . runReaderT (interp entry) $ ast
+  execVarState vtab . runReaderT (interp [] entry) $ ast
 
 -- ======
 -- Blocks
 -- ======
 
-interp :: Label -> ProgState ()
-interp l = ask >>= \ast -> let (Just (_,ss,t)) = lookup l ast in do
+interp :: Label -> Label -> ProgState ()
+interp from l = do
+
+  Just (f,ss,t) <- asks (lookup l)
+
+  case f of
+    Entry _      -> return ()
+    From l' p    -> unless (from == l') $
+      lift (logError $ RuntimeError p (CustomRT "From not consistent."))
+    Fi a l1 l2 p -> do
+      a' <- lift $ valToBool <$> eval a
+      unless (from == (if a' then l1 else l2)) $
+        lift (logError $ RuntimeError p (CustomRT $ "From not consistent. Coming from " ++ from ++ "."))
+
   lift $ execStmts ss
+
   case t of
-    Exit _     -> return ()
-    Goto l _     -> interp l
-    IfTo t l1 l2 p -> do
+    Exit _         -> return ()
+    Goto l' _      -> interp l l'
+    IfTo t l1 l2 _ -> do
       t' <- lift $ valToBool <$> eval t
-      if t' then interp l1 else interp l2
+      if t' then interp l l1 else interp l l2
