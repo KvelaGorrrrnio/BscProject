b362db746e9131e4b1e6fcfa8b7a737827c9c89c
Lars <hnd531@alumni.ku.dk>
2018-05-12 23:14:36 +0200

    Update.

--- src/src/SRL/Optimise.hs
+++ src/src/SRL/Optimise.hs
@@ -5,9 +5,12 @@ import SRL.Error
 
 import Common.Optimise
 
+import Debug.Trace
+
 optimise :: Either Error (TypeTab,AST) -> Either Error (TypeTab,AST)
 optimise (Left err) = Left err
 optimise (Right (ttab,ast)) | ast' <- optStmts ast =
+  trace (showAST ttab ast' ++ "\n\n") $
   if ast' == ast
   then Right (ttab,ast')
   else optimise $ Right (ttab,ast')
